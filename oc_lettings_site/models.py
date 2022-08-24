d6408b85be4140affeb55053ddca949c0c7aafc1
byoso <emailvinz@gmail.com>
2022-08-24 16:38:53 +0200

    Profile ready for data copy

--- oc_lettings_site/models.py
+++ oc_lettings_site/models.py
@@ -27,7 +27,7 @@ class Letting(models.Model):
         return self.title
 
 
-class Profile(models.Model):
+class OldProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     favorite_city = models.CharField(max_length=64, blank=True)
 
--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
 
     # Local
     'lettings',
+    'profiles',
 ]
 
 MIDDLEWARE = [
