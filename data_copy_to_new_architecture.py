213cd64c8c6fb6329d633fe52649c0309aeadc06
byoso <emailvinz@gmail.com>
2022-08-24 17:02:51 +0200

    datas split in two apps: 'profiles' and 'lettings' done, TODO: split the templates as well

--- data_copy_to_new_architecture.py
+++ data_copy_to_new_architecture.py
@@ -1,6 +1,20 @@
 import lettings
+import profiles
 import oc_lettings_site
 
+"""WARNING: DO NOT RUN THIS SCRIPT (it has already be done)
+Some datas will be doubled, this is probably not what you want.
+If you go for it anyway, uncomment the code and run it in a django shell:
+
+$ ./manage.py shell < data_copy_to_new_architecture.py
+
+"""
+
+print(
+    "This script is not expected to be run twice, you should not do it.\n"
+    "If for some reason you have to run it, read it's docstring first, and \n"
+    "manually uncomment it's content."
+    )
 
 # old_addresses = oc_lettings_site.models.Address.objects.all()
 
@@ -25,4 +39,15 @@ import oc_lettings_site
 #     )
 #     new.save()
 
-print("Script executed: copied old architecture's database tables to new database tables")
+# old_profiles = oc_lettings_site.models.OldProfile.objects.all()
+# for profile in old_profiles:
+#     print(profile.id, profile.favorite_city, profile.user_id)
+#     new_profile = profiles.models.Profile(
+#         id=profile.id,
+#         favorite_city=profile.favorite_city,
+#         user_id=profile.user_id
+#     )
+#     print("new profile :", new_profile.id, new_profile.favorite_city, new_profile.user_id)
+#     new_profile.save()
+
+# print("Script executed: copied old architecture's database tables to new database tables")
