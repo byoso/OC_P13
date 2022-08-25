04974d3c18122ec8d058675f9e27e678fad7b0b9
byoso <emailvinz@gmail.com>
2022-08-25 20:39:47 +0200

    namespaces and templates updated, TODO: move oc_lettings_site into an application

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-import profiles
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('lettings/', include('lettings.urls')),
-    path('profiles/', include('profiles.urls')),
+    path('lettings/', include('lettings.urls', namespace='lettings')),
+    path('profiles/', include('profiles.urls', namespace='profiles')),
     path('admin/', admin.site.urls),
 ]
