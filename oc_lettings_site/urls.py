22971a7ab6161acb5d0d8c139cab64ec4616ba9b
byoso <emailvinz@gmail.com>
2022-09-29 14:59:35 +0200

    added sentry to the project

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 
 
+# sentry installation test
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
     path('', include('home.urls', namespace='home')),
     path('lettings/', include('lettings.urls', namespace='lettings')),
     path('profiles/', include('profiles.urls', namespace='profiles')),
     path('admin/', admin.site.urls),
+    path('sentry-debug/', trigger_error),  # sentry installation test
 ]
