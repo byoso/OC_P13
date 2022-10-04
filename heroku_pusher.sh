3a2b9a2b2c4f9cf1266bf31f2518e66888be40b2
byoso <emailvinz@gmail.com>
2022-10-04 16:00:29 +0200

    heroku pusher script is working

--- heroku_pusher.sh
+++ heroku_pusher.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
 
+# before running this script, be sure you have a functionnal
+# docker container ready
+# $ docker container ps -a
+
 heroku login
 heroku container:login
 heroku create oc-lettings-vf
 heroku git:remote -a oc-lettings-vf
-heroku container: push web
-heroku container: release web
+heroku container:push web
+heroku container:release web
