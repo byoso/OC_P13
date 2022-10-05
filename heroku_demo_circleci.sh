2f3c3656fa0c655387b597fa9c21a7d30b9d3d5f
byoso <emailvinz@gmail.com>
2022-10-05 19:58:46 +0200

    heroku_demo_cirleci.sh improved

--- heroku_demo_circleci.sh
+++ heroku_demo_circleci.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
 
+# this demonstrate how to recreate the site if completely deleted from
+# heroku
 
 heroku login
 heroku container:login
 heroku create oc-lettings-vf
 
 ./heroku_environment.sh
+git commit --allow-empty -am 'empty commit'
+git push
